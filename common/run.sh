2dde7cb96997a9708e5bccd0f4e1bce12ba7af95
Marius Ciotlos <ciotlos@gmail.com>
2020-09-28 23:42:08 +0300

    Updated run.sh with more bashio

--- common/run.sh
+++ common/run.sh
@@ -1,43 +1,41 @@
 #!/bin/bash
-
-CONFIG_PATH=/data/options.json
-
-DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
+DATA_PATH=$(bashio::config 'data_path')
+ZIGBEE_HERDSMAN_DEBUG=$(bashio::config 'zigbee_herdsman_debug')
+ZIGBEE_SHEPHERD_DEVICES=$(bashio::config 'zigbee_shepherd_devices')
 DEBUG=""
-ZIGBEE_HERDSMAN_DEBUG=$(jq --raw-output ".zigbee_herdsman_debug // empty" $CONFIG_PATH)
-ZIGBEE_SHEPHERD_DEVICES=$(jq --raw-output ".zigbee_shepherd_devices // empty" $CONFIG_PATH)
 
-# Check if config exists already
-mkdir -p $DATA_PATH
-if [[ -f $DATA_PATH/configuration.yaml ]]; then
-    if [[ ! -f $DATA_PATH/.configuration.yaml.bk ]]; then
-        echo "[Info] Configuration file found in data path, but no backup file found in data path. Backing up existing configuration to ${DATA_PATH}/.configuration.yaml.bk"
-        cp $DATA_PATH/configuration.yaml $DATA_PATH/.configuration.yaml.bk
-    else
-        "[Info] Configuration backup found in ${DATA_PATH}/.configuration.yaml.bk. Skipping config backup."
-    fi
+if ! bashio::fs.directory_exists $DATA_PATH; then
+    bashio::log.info "$DATA_PATH not present, probably first run, creating folder ..."
+    mkdir -p $DATA_PATH
 fi
 
+if bashio::fs.fs.file_exists $DATA_PATH/configuration.yaml && !bashio::fs.fs.file_exists $DATA_PATH/configuration.yaml.bk; then
+    bashio::log.info "Config 'configuration.yaml' found, but backup is missing. Creating backup '.configuration.yaml.bk' ..."
+    cp $DATA_PATH/configuration.yaml $DATA_PATH/.configuration.yaml.bk
+fi
+
+if bashio::config.true 'zigbee_herdsman_debug'; then
+    bashio::log.info "Zigbee Herdsman debug logging enabled"
+    DEBUG="zigbee-herdsman:*"
+if
+
+if bashio::config.true 'zigbee_shepherd_devices'; then
+    bashio::log.info "Searching for custom devices file in zigbee2mqtt data path..."
+    if bashio::fs.fs.file_exists $DATA_PATH/devices.js; then
+        ashio::log.info "File devices.js found, copying to ./node_modules/zigbee-herdsman-converters/ ..."
+        cp -f "$DATA_PATH"/devices.js ./node_modules/zigbee-herdsman-converters/devices.js
+    else
+        bashio::log.warn "No devices.js file found in data path, starting with default devices.js ..."
+    fi
+if
+
+CONFIG_PATH=/data/options.json
 # Parse config
 cat "$CONFIG_PATH" | jq 'del(.data_path, .zigbee_shepherd_debug, .zigbee_shepherd_devices, .socat)' \
     | jq 'if .advanced.ext_pan_id_string then .advanced.ext_pan_id = (.advanced.ext_pan_id_string | (split(",")|map(tonumber))) | del(.advanced.ext_pan_id_string) else . end' \
     | jq 'if .advanced.network_key_string then .advanced.network_key = (.advanced.network_key_string | (split(",")|map(tonumber))) | del(.advanced.network_key_string) else . end' \
     | jq 'if .device_options_string then .device_options = (.device_options_string|fromjson) | del(.device_options_string) else . end' \
     > $DATA_PATH/configuration.yaml
-
-if [[ ! -z "$ZIGBEE_HERDSMAN_DEBUG" ]]; then
-    echo "[Info] Zigbee Herdsman debug logging enabled."
-    DEBUG="zigbee-herdsman:*"
-fi
-
-if [[ ! -z "$ZIGBEE_SHEPHERD_DEVICES" ]]; then
-    echo "[Info] Searching for custom devices file in zigbee2mqtt data path..." 
-    if [[ -f "$DATA_PATH"/devices.js ]]; then
-        cp -f "$DATA_PATH"/devices.js ./node_modules/zigbee-herdsman-converters/devices.js
-    else
-        echo "[Error] File $DATA_PATH/devices.js not found! Starting with default devices.js"
-    fi
-fi
 
 # FORK SOCAT IN A SEPARATE PROCESS IF ENABLED
 SOCAT_EXEC="$(dirname $0)/socat.sh"
